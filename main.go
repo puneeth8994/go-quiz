33acba45ee17dcf447478c5b7ca5e59691ee2578
puneeth.s <3094-puneeth.s@users.noreply.source.golabs.io>
2020-08-04 13:49:25 +0530

    Added a timer feature to time the quiz

--- main.go
+++ main.go
@@ -5,12 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
 
 	//adds csv as a flag
 	csvFileName := flag.String("csv", "problems.csv", "A csv file in the format of 'question,answer'")
+	timeLimit := flag.Int("limit", 30, "The time limit for the quiz in seconds")
 	flag.Parse()
 
 	file, err := os.Open(*csvFileName)
@@ -29,18 +31,34 @@ func main() {
 
 	problems := parseLines(lines)
 
+	//timer sends message through a channel once the provided duration is completed.
+	//This will help us keep a track of the quiz time.
+	timer := time.NewTimer(time.Duration(*timeLimit) * time.Second)
+
 	correctAnswers := 0
 	for i, p := range problems {
-		fmt.Printf("Problem #%d: %s = \n", i+1, p.Q)
-		var answer string
-		fmt.Scanf("%s\n", &answer)
+		fmt.Printf("Problem #%d: %s = ", i+1, p.Q)
 
-		if answer == p.A {
-			correctAnswers++
+		answerCh := make(chan string)
+		//Anonymous function running which scans the answer.
+		go func() {
+			var answer string
+			fmt.Scanf("%s\n", &answer)
+			answerCh <- answer
+		}()
+
+		select {
+		case <-timer.C:
+			fmt.Printf("\nYou scored %d out of %d.\n", correctAnswers, len(problems))
+			return
+		case answer := <-answerCh:
+			if answer == p.A {
+				correctAnswers++
+			}
+		default:
 		}
 	}
-
-	fmt.Printf("You scored %d out of %d.\n", correctAnswers, len(problems))
+	fmt.Printf("\nYou scored %d out of %d.\n", correctAnswers, len(problems))
 }
 
 //Problem represents a struct of question and answer
